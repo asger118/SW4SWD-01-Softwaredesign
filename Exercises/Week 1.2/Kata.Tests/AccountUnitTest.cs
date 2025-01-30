94d136422ba95ff8e4b98f006d71bfe06000c35d
AsgerVeng <Asgerveng1@gmail.com>
2025-01-30 13:45:48 +0100

    initial commit of banksystem

---
+++ Exercises/Week 1.2/Kata.Tests/AccountUnitTest.cs
@@ -0,0 +1,103 @@
+﻿namespace Kata.Tests;
+
+public class AccountUnitTest
+{
+    [Theory]
+    [InlineData(1000, 500)]
+    [InlineData(1000, 1000)]
+    [InlineData(1000, 99.99)]
+    public void WithdrawTest_ValidInput(double accontBalance, double amount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+
+        // Act
+        account.Withdraw(amount);
+
+        // Assert
+        Assert.Equal(accontBalance - amount, account.Balance);
+    }
+
+    [Theory]
+    [InlineData(1000, 0)]
+    [InlineData(1000, -500)]
+    [InlineData(1000, -500.99)]
+    [InlineData(1000, 1500)]
+    [InlineData(1000, 1500.99)]
+    public void WithdrawTest_InvalidInput(double accontBalance, double amount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+
+        // Act
+        Action act = () => account.Withdraw(amount);
+
+        // Assert
+        Assert.Throws<ArgumentException>(act);
+    }
+
+    [Theory]
+    [InlineData(1000, 500)]
+    [InlineData(1000, 500.10)]
+    [InlineData(1000, 999)]
+    public void DepositTest_ValidInput(double accontBalance, double depositAmount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+
+        // Act
+        account.Deposit(depositAmount);
+
+        // Assert
+        Assert.Equal(accontBalance + depositAmount, account.Balance);
+    }
+
+    [Theory]
+    [InlineData(1000, -500)]
+    [InlineData(1000, 0)]
+    public void DepositTest_InvalidInput(double accontBalance, double depositAmount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+
+        // Act
+        Action act = () => account.Deposit(depositAmount);
+
+        // Assert
+        Assert.Throws<ArgumentException>(act);
+    }
+
+    [Theory]
+    [InlineData(100, 100, 50)]
+    [InlineData(100, 100, 100)]
+    public void TransferTest_ValidInput(double accontBalance, double targetAccountBalance, double transferAmount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+        var targetAccount = new Account(targetAccountBalance);
+
+        // Act
+        account.Transfer(transferAmount, targetAccount);
+
+        // Assert
+        Assert.Equal(accontBalance - transferAmount, account.Balance);
+        Assert.Equal(targetAccountBalance + transferAmount, targetAccount.Balance);
+    }
+
+    [Theory]
+    [InlineData(100, 100, -100)]
+    [InlineData(100, 100, 200)]
+    public void TransferTest_InvalidInput(double accontBalance, double targetAccountBalance, double transferAmount)
+    {
+        // Arrange
+        var account = new Account(accontBalance);
+        var targetAccount = new Account(targetAccountBalance);
+
+        // Act
+        Action act = () => account.Transfer(transferAmount, targetAccount);
+
+        // Assert
+        Assert.Throws<ArgumentException>(act);
+    }
+
+}
---
+++ Exercises/Week 1.2/Kata/Account.cs
@@ -0,0 +1,52 @@
+﻿namespace Kata;
+
+public class Account
+{
+    public double Balance { get; private set; }
+
+    public Account(double balance)
+    {
+        Balance = balance;
+    }
+
+    public double Withdraw(double amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Amount must be positive");
+        }
+        else if (Balance < amount)
+        {
+            throw new ArgumentException("Insufficient funds");
+        }
+
+        Balance -= amount;
+
+        return amount;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Amount must be positive");
+        }
+        else if (amount == 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero");
+        }
+
+        Balance += amount;
+    }
+
+    public void Transfer(double amount, Account targetAccount)
+    {
+        if (targetAccount == null)
+        {
+            throw new ArgumentNullException(nameof(targetAccount));
+        }
+
+        targetAccount.Deposit(Withdraw(amount));
+    }
+}
+
